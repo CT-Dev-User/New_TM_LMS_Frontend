fe66cdce31d0f74b2b878e5c60718dcc2cd347ee
vaibhavi24 <mahurevaibhavi16@gmail.com>
2025-07-04 13:46:40 +0530

    updated syntzx of vite

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,3 @@
-{
-  "devDependencies": {
-    "@vitejs/plugin-react": "^4.0.0"
-  }
-}
-The issue: Your vite.config.js is importing the wrong plugin name. You have @vitejs/plugin-react-swc installed (which is faster), but your config is trying to import @vitejs/plugin-react.
-
-Update your vite.config.js and push the changes - this should fix the build!
-
-
-
-
-
-
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
